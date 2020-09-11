9d1ed2e754f733ef043a18b59eb2a2f4ef79c6b8
duvanjm <duvanjarin@gmail.com>
2020-09-11 04:54:01 +0000

    more tests

--- tests/test_models/test_engine/test_db_storage.py
+++ tests/test_models/test_engine/test_db_storage.py
@@ -157,3 +157,12 @@ class TestDBStorageMethod(unittest.TestCase):
         result = storage.all("State")
         count = storage.count("State")
         self.assertEqual(len(result), count)
+
+    @unittest.skipIf(models.storage_t != 'db', "not testing db storage")
+    def test_get_file_storage(self):
+        """ test """
+        fs = FileStorage()
+        new_state = State()
+        fs.new(new_state)
+        first_state_id = list(storage.all("State").values())[0].id
+        self.assertEqual(type(storage.get("State", first_state_id)), State)
